cc5d9437a9977bfe6391ab78106f8afeb55e2131
SirraRakesh <sirrarakesh2000@gmail.com>
2022-11-11 12:47:20 +0530

    add form folder

---
+++ Forms.js
@@ -0,0 +1,13 @@
+const form=document.getElementsByClassName('form')
+const uname=document.getElementsByClassName('uname')
+const  mail=document.getElementsByClassName('mail')
+const pass=document.getElementsByClassName('pass')
+const cpass=document.getElementsByClassName('cpass')
+const clg=document.getElementsByClassName('clg')
+const sub=document.getElementsByClassName('submit')
+form.addEventListener('sumbit',(e)=>{
+    e.preventDefault()
+    validate()
+})
+function
+
